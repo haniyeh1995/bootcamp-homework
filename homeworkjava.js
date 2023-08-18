5d96277c24beba875ac044a2b5b6a7eb3b98e662
haniyeh1995 <142569710+haniyeh1995@users.noreply.github.com>
2023-08-18 23:31:03 +0330

    Add files via upload

---
+++ homeworkjava.js
@@ -0,0 +1,14 @@
+let numberID = prompt('please enter your numberID');
+alert("your numberID is" + " " + numberID);
+
+for ( i = 0; i < numberID; i+= 2 ){
+    console.log(i);   
+}
+
+
+
+const fullName = prompt ('please enter your name');
+alert("your name is" + " " + fullName);
+
+nameLength = fullName.length;
+console.log(nameLength);
