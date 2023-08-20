ca56d85e0e40dcee50cf1f1925e2b4e3c3293361
haniyeh1995 <142569710+haniyeh1995@users.noreply.github.com>
2023-08-20 23:49:54 +0330

    Create java-homework.js

---
+++ java-homework.js
@@ -0,0 +1,55 @@
+// 1
+// let n = prompt('please enter your number');
+
+// function prime(n) {
+//     if (n === 1) {
+//         return false;
+//     } else if (n === 2) {
+//         return true;
+//     } else {
+//         for (let i = 2; i < n; i++) {
+//             if (n % i === 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+// console.log(prime(n));
+
+
+// 2
+// const userName = prompt('please enter your name');
+// const getUserName = userName.split('').reverse('').join('');
+// console.log(getUserName);
+
+// 3
+// let userNumber = prompt("please enter your number");
+// let getNumber = [];
+// for  (i = 0; i <= userNumber; ) {
+//     getNumber.push(i);
+// }
+// console.log(getNumber.length);
+
+// const Pythagorean=Math.sqrt(a ** 2) + (b ** 2) = (c ** 2);
+
+// let pythagoreanNumber = prompt("please enter your number");
+
+// function pythagorean_theorem(x, y) {
+//     if ((typeof x !== 'number') || (typeof y !== 'number')) 
+//   return false; 
+//           return Math.sqrt(x * x + y * y);
+//       }
+  
+//   console.log(pythagorean_theorem(x, y));
+
+// 4
+// const nickName = prompt('please enter your nickname');
+// // const repeatCharacter = nickName.toString.repeat(1);
+// // console.log(repeatCharacter);
+// const tostring = String(nickName);
+
+
+// const repeatCharacter =repeatStringNumTimes("tostring", 1);
+
+// console.log(repeatCharacter);
