770e36d24e6e2be28ef533173784860d1af9e6e9
haniyeh nematollahi <h.nematollahi@TAKHFIFAN.THR>
2023-09-26 21:31:37 +0330

    dsd

---
+++ react homework week6/src/header/contenticon/contenticon.jsx
@@ -0,0 +1,18 @@
+import './contenticon.css'
+import Content from './content'
+
+function Contenticon(props) {
+    const content = props.content
+    return (
+        <>
+            <div className='content-icon'>
+                <Content content={content} />
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
+                    <path d="M16.6 7.95834L11.1667 13.3917C10.525 14.0333 9.47503 14.0333 8.83336 13.3917L3.40002 7.95834" stroke="#5B5D67" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+            </div>
+        </>
+    )
+}
+export default Contenticon 
